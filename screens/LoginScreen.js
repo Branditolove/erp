26b700127871eed8009162c853feca864e788a1d
Ricardo Angel Fournier Delgado <75813716+RicardoAngelFournier@users.noreply.github.com>
2024-02-07 11:37:04 -0600

    ger

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -14,7 +14,7 @@ const LoginScreen = () => {
 
   const handleLogin = async () => {
     try {
-      const response = await axios.post('http://192.168.151.244:8000/api/v1/login', {
+      const response = await axios.post('http://192.168.151.194:8000/api/v1/login', {
         email: email,
         password: password,
       });
